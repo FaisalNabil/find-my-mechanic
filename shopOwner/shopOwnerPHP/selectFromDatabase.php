f92a23e26293e1bccb321e43cd0f5a012ca7b194
sarwarhosen007 <sarwarhosen007@yahoo.com>
2017-04-06 22:11:36 +0600

    Modified ShopOwner files

--- shopOwner/shopOwnerPHP/selectFromDatabase.php
+++ shopOwner/shopOwnerPHP/selectFromDatabase.php
@@ -1,7 +1,11 @@
 <?php
 function getJSONFromDB($sql){
-	$conn = mysqli_connect("localhost", "root", "","find_my_mechanic");
 	
+	$conn = mysqli_connect("localhost", "root", "root", "find_My_Mechanic");
+    if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+    }
+
 	$result = mysqli_query($conn, $sql)or die(mysqli_error($conn));
 	$arr=array();
 	while($row = mysqli_fetch_assoc($result)) {
