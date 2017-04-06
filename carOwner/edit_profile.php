38647cd6ee0f18d4bf36e6fd6814ea72f2ae8f3d
sarwarhosen007 <sarwarhosen007@yahoo.com>
2017-04-06 10:42:56 +0600

    Modified EditProfile.php page

--- carOwner/edit_profile.php
+++ carOwner/edit_profile.php
@@ -195,8 +195,43 @@ $carOwnerData = json_decode($jsonString);
             </div>
             <!-- end sidebar-collapse -->
         </nav>
-        <!-- end navbar side -->
-        <!--  page-wrapper -->
+         <?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    include 'phpFiles/Mysqldb.php';
+
+   if (!empty($_POST['Name']) && !empty($_POST['Birthdate']) && !empty($_POST['contact']) && !empty($_POST['NID']) && !empty($_POST['DrivingLicence']) && !empty($_POST['PresentAddress']))
+
+    {
+      $Name           = $_POST['Name'];  
+      $Birthdate      = $_POST['Birthdate'];    
+      $contact        = $_POST['contact'];    
+      $NID            = $_POST['NID'];  
+      $DrivingLicence = $_POST['DrivingLicence'];   
+      $PresentAddress = $_POST['PresentAddress'];   
+    }
+       
+
+       $sql = "UPDATE carowner SET Name ='".$Name."',Contact='".$contact."',DOB='".$Birthdate."',NID='".$NID."',DrivingLicence = '".$DrivingLicence."',Address='".$PresentAddress."' WHERE Email='nabilt59@gmail.com'"; 
+
+        if (mysqli_query($conn, $sql)) {
+            $info = 
+            '<div class="alert alert-success alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Success!</strong> The Value Supdated Successfully.
+             </div>';
+        } else {
+            $info = 
+            '<div class="alert alert-info alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Info!</strong> Updated Failed.
+             </div>';
+        }
+
+}
+
+?>
          <div id="page-wrapper">
             <div class="row">
                 <!-- Page Header -->
@@ -204,6 +239,11 @@ $carOwnerData = json_decode($jsonString);
                     <h1 class="page-header">Profile</h1>
                 </div>
                 <!--End Page Header -->
+            </div>
+            <div class="cow">
+                <div class="col-lg-12">
+                    <?php echo $info; ?>
+                </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
@@ -216,44 +256,44 @@ $carOwnerData = json_decode($jsonString);
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="table-responsive">
-                                         <form class="form-horizontal">
+                                         <form class="form-horizontal" action="" method="post">
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="email">Name:</label>
                                                 <div class="col-sm-5">
-                                                  <input type="text" class="form-control" id="name" placeholder="" value="<?php echo $carOwnerData[0]->Name ;?>">
+                                                  <input type="text" class="form-control" id="Name" name="Name" value="<?php echo $carOwnerData[0]->Name ;?>">
                                                 </div>
                                               </div>
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="DOB">DOB:</label>
                                                 <div class="col-sm-5">
-                                                  <input type="text" class="form-control" id="popupDatepicker" placeholder="Please Select Your Date of Birth" value="<?php echo $carOwnerData[0]->DOB ;?>">
+                                                  <input type="text" class="form-control" id="popupDatepicker" placeholder="Please Select Your Date of Birth" name="Birthdate" value="<?php echo $carOwnerData[0]->DOB ;?>">
                                                 </div>
                                               </div>
 
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="email">Contact:</label>
                                                 <div class="col-sm-5">
-                                                  <input type="text" class="form-control" id="contact" placeholder="" value="<?php echo $carOwnerData[0]->Contact ;?>">
+                                                  <input type="text" class="form-control" id="contact" placeholder="" name="contact"  value="<?php echo $carOwnerData[0]->Contact ;?>">
                                                 </div>
                                               </div>
 
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="nid">NID:</label>
                                                 <div class="col-sm-5">
-                                                  <input type="text" class="form-control" id="nid" value="<?php echo $carOwnerData[0]->NID ;?>">
+                                                  <input type="text" class="form-control" name="NID" id="nid" value="<?php echo $carOwnerData[0]->NID ;?>">
                                                 </div>
                                               </div>
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="dlc">Driving Liscence:</label>
                                                 <div class="col-sm-5">
-                                                  <input type="text" class="form-control" id="dlc" value="<?php echo $carOwnerData[0]->{'Driving Licence'} ;?>">
+                                                  <input type="text" class="form-control" name="DrivingLicence" id="dlc" value="<?php echo $carOwnerData[0]->{'Driving Licence'} ;?>">
                                                 </div>
                                               </div>
 
                                               <div class="form-group">
                                                 <label class="control-label col-sm-2" for="address">Present Address:</label>
                                                 <div class="col-sm-5">
-                                                   <textarea class="form-control" id="comment" required=""><?php echo $carOwnerData[0]->Address ;?></textarea>
+                                                   <textarea class="form-control" id="comment" name="PresentAddress" required=""><?php echo $carOwnerData[0]->Address ;?></textarea>
                                                 </div>
                                               </div>
 
