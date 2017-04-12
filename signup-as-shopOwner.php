71634b27e2434683d4db085d0f3b62c822e75271
Safaet Hossain <tuhin_42@hotmail.com>
2017-04-12 09:15:17 +0600

    modified few files

--- signup-as-shopOwner.php
+++ signup-as-shopOwner.php
@@ -8,10 +8,71 @@
     <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
 
-   <link href="assets/css/style.css" rel="stylesheet" />
-   <link href="assets/css/main-style.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/main-style.css" rel="stylesheet" />
 
 </head>
+<?php
+  $nameError = $phoneError = $emailError = $passError = $conPassError = "";
+  $error = 0;
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    if(!preg_match("/^[a-zA-Z ]*$/", $_POST["shopOwnerName"])){
+      $error = 1;
+      $nameError = "Only letters and space are allowed";
+    }else{
+      $name = $_POST["shopOwnerName"];
+    }
+
+    if (!filter_var($_POST["shopOwnerEmail"], FILTER_VALIDATE_EMAIL)){
+      $error +=1;
+      $emailError = "Invalid email format";
+    }else{
+      $email = $_POST["shopOwnerEmail"];
+    }
+
+    if(!preg_match("/^[0-9]*$/",$_POST["shopOwnerPhone"])){
+      $error += 1;
+      $phoneError = "only numbers are allowed";
+    }else{
+      $phone = $_POST["shopOwnerPhone"];
+    }
+
+    $password = $_POST["shopOwnerPWD"];
+
+    if($password != $_POST["shopOwnerCPWD"]){
+      $error +=1;
+      $conPassError = "Password not match";
+    }
+
+    $stl = $_POST["shopOwnerTDN"];
+
+    $latitude = $_POST["shoOwnerLatitude"];
+
+    $longitude = $_POST["shopOwnerLongitude"];
+
+    $address = $_POST["shopOwnerAddress"];
+   
+    if($error == 0){
+      $conn = mysqli_connect("localhost", "root", "","find_my_mechanic");
+      if(!$conn){
+        die("connection failed: ".mysqli_connect_error());
+      }
+      $sql = "insert into shopowner(ShopName,Email,Contact,Password,Latitude,Longitude,Address,ShopTradeLicence,flag) values ('".$name."','".$email."','".$phone."','".$password."','".$latitude."','".$longitude."','".$address."','".$stl."',2);";
+
+      $sql .= "insert into carshop(Email,Password,flag) values ('".$email."','".$password."',2);";
+
+      if (mysqli_multi_query($conn, $sql)) {
+        $_SESSION["shopOwnerSignupEmail"] = $email;
+          header("Location: shopOwner/index.html");
+      }else{
+          echo mysqli_error($conn);
+      }
+
+      mysqli_close($conn);
+    }
+  }
+?>
 
 <body class="body-Login-back">
 
@@ -24,37 +85,37 @@
                         <h3 class="panel-title">Please Enter Your information</h3>
                     </div>
                     <div class="panel-body">
-                          <form class="form-horizontal" action = "shopAction.php" method = "Post">
+                          <form class="form-horizontal" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "Post">
 
                              <div class="form-group">
                                 <label class="control-label col-sm-2" for="shpName">Shop Name:</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control" id="shpName" name = "shopOwnerName" placeholder="Enter Shop Name" required>
+                                  <input type="text" class="form-control" id="shpName" name = "shopOwnerName" placeholder="Enter Shop Name" required><span><?php echo $nameError; ?></span>
                                 </div>
                               </div>
 
                               <div class="form-group">
                                 <label class="control-label col-sm-2" for="email">Email:</label>
                                 <div class="col-sm-10">
-                                  <input type="email" class="form-control" id="email" name = "shopOwnerEmail" placeholder="Enter email" required>
+                                  <input type="email" class="form-control" id="email" name = "shopOwnerEmail" placeholder="Enter email" required><span><?php echo $emailError; ?></span>
                                 </div>
                               </div>
                               <div class="form-group">
                                 <label class="control-label col-sm-2" for="pwd">Contact:</label>
                                 <div class="col-sm-10">
-                                  <input type="text" class="form-control" id="pwd" name = "shopOwnerPhone" placeholder="Enter Contact Number" required>
+                                  <input type="text" class="form-control" id="pwd" name = "shopOwnerPhone" placeholder="Enter Contact Number" required><span><?php echo $phoneError; ?></span>
                                 </div>
                               </div>
                               <div class="form-group">
                                 <label class="control-label col-sm-2" for="pwd">Password:</label>
                                 <div class="col-sm-10">
-                                  <input type="password" class="form-control" id="pwd" name = "shopOwnerPWD" placeholder="Enter Password" required>
+                                  <input type="password" class="form-control" id="pwd" name = "shopOwnerPWD" placeholder="Enter Password" required><span><?php echo $passError; ?></span>
                                 </div>
                               </div>
                               <div class="form-group">
                                 <label class="control-label col-sm-2" for="pwd">Confirm Password:</label>
                                 <div class="col-sm-10">
-                                  <input type="password" class="form-control" id="pwd" placeholder="Enter Password Again" required>
+                                  <input type="password" class="form-control" id="pwd" name = "shopOwnerCPWD" placeholder="Enter Password Again" required><span><?php echo $conPassError; ?></span>
                                 </div>
                               </div>
 
