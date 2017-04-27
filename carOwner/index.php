3680e9e93c086c1615ee3360374989f3b77aa579
sarwarhosen007 <sarwarhosen007@yahoo.com>
2017-04-28 00:56:25 +0600

    Add a new file and modified some file

--- carOwner/index.php
+++ carOwner/index.php
@@ -1,6 +1,5 @@
 <?php 
       $currentPage = 'home';
-      $info="";
    include 'TemplateFile/header.php'; 
 
    if(isset($_POST['send']) && $_SERVER["REQUEST_METHOD"] == "POST"){
@@ -9,8 +8,7 @@
       $lat=$_POST['lat'];
       $long=$_POST['long'];
       $email=$_POST['email'];
-      $var=date("md")+time();
-      $serviceId=substr($_SESSION["carOwnerEmail"], 0,1).substr($email, 0,1).(string)$var;
+      $serviceId=substr($_SESSION["carOwnerEmail"], 0,1).substr($email, 0,1).date("Y-m-d");
 
 
       require ("phpFiles/updateDatabase.php");
@@ -23,23 +21,14 @@
 
       //echo $notificationsql;
       if(updateDB($sql)==1){
-        if(updateDB($notificationsql)==1){
-          $info=
-                    '<div class="alert alert-success alert-dismissable">
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <strong>Request Sent Successfully</strong>
-                     </div>';
-          if($messageBody!=""){
-            updateDB($msgsql);
+        if(updateDB($msgsql)==1){
+          if(updateDB($notificationsql)==1){
+            $info="";
           }
         }
       }
       else{
-          $info=
-                    '<div class="alert alert-danger alert-dismissable">
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <strong>Request Sending was Unsuccessfull</strong>
-                     </div>';
+          $info="";
       }
       
     }
@@ -134,7 +123,7 @@
                 <!-- Welcome -->
                 <div class="col-lg-12 text-center">
                     <div class="alert alert-info">
-                        <i class="fa fa-folder-open"></i><b>&nbsp;Hello ! </b>Welcome Back <b><?php echo $carOwnerData[0]->Name; ?></b>
+                        <i class="fa fa-folder-open"></i><b>&nbsp;Hello ! </b>Welcome Back <b>Faisal Nabil </b>
                         <i class="fa fa-map-marker"></i> <b>&nbsp; Now you are in <span class="location-color">
 
                         <?php 
@@ -175,7 +164,7 @@
 
             <div class="row">
                 <div class="col-lg-6 col-lg-offset-2">
-                    <?php echo $info ; 
+                    <?php //echo $info ; 
                     ?>
                 </div>
             </div>
@@ -211,7 +200,7 @@
                                                 <tr>
                                                     <td><?php echo ($i+1) ; ?></td>
                                                     <td><?php echo $ShopOwnerData[$i]->ShopName ;?></td>
-                                                    <td><?php echo sprintf('%0.2f',$km[$i]); ?> km</td>
+                                                    <td><?php echo sprintf('%0.2f',$km[$i]); ?></td>
                                                     <td><button class="btn btn-success" data-toggle="modal" data-target="#requestSendModal<?php echo $i ?>">Send Request</button>
                                                <!-- Modal -->
   <div class="modal fade" id="requestSendModal<?php echo $i ?>" role="dialog">
